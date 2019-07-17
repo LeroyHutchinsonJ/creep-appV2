72609d8fce407c913a96927fe2b1122757ddcf7b
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-07-16 23:12:44 -0400

    Added the neccesary dependencies to start using redux in my application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './InputBox.js';
+import InputBox from './InputBox.js';
+//The provider connects the store to react
+import {Provider} from 'react-redux';
+//The store is where all of the state stuff is stored
+import {Store} from "redux";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+         <InputBox/>
+          <div className="Details">
+            <div>Hey</div>
+
+            </div>
     </div>
-  );
+  )
 }
 
 export default App;
